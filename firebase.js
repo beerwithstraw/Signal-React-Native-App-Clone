22ed0c20eedbf516f433d0ebd6ccd739643fc24a
beerwithstraw <agarwalp@tcd.ie>
2021-01-28 20:14:34 +0000

    Firebase Config

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDGKfp_6BLbJWo9CHmc0FWngGUJ4PNzlgo",
+    authDomain: "signal-react-native-clone.firebaseapp.com",
+    projectId: "signal-react-native-clone",
+    storageBucket: "signal-react-native-clone.appspot.com",
+    messagingSenderId: "345481180431",
+    appId: "1:345481180431:web:a78820ad0b773c8d087a7a"
+  };
