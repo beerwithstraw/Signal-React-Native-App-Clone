52243dbbd383ae3e090dddcefad4a2220ffe5d2a
beerwithstraw <agarwalp@tcd.ie>
2021-01-28 23:30:56 +0000

    Registration

---
+++ screens/RegisterScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const RegisterScreen = () => {
+    return (
+        <View>
+            <Text>This is Registration Screen!</Text>
+        </View>
+    )
+}
+
+export default RegisterScreen
+
+const styles = StyleSheet.create({})
