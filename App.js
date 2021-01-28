08e030b857c765d48cca244caa794005a6ddfd3d
beerwithstraw <agarwalp@tcd.ie>
2021-01-28 20:37:24 +0000

    Basic Login Stack

--- App.js
+++ App.js
@@ -3,15 +3,26 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+import LoginScreen from './screens/LoginScreen';
 
+const Stack = createStackNavigator(); 
+const globalScreenOptions = {
+  headerStyle: {backgroundColor: "#2C6BED"},
+  headerTitleScreen: {color: "white"},
+  headerTintColor: "white"
+}
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen 
+          options={{
+            title: "Sign Up!"
+          }}
+          name='Login' component={LoginScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
 
   );
