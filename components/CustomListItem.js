d0dee8e974645f4e1d80d72ebb70c1f22c38ef21
beerwithstraw <agarwalp@tcd.ie>
2021-01-29 00:56:37 +0000

    Homepage Chat Item List

---
+++ components/CustomListItem.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { ListItem, Avatar } from 'react-native-elements'
+
+const CustomListItem = () => {
+    return (
+        <ListItem>
+            <Avatar 
+                rounded
+                source={{
+                    uri: "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
+                }}
+            />
+
+            <ListItem.Content>
+                <ListItem.Title style={{fontWeight: "600"}}>
+                    Group Project Chat
+                </ListItem.Title>
+            </ListItem.Content>
+
+        </ListItem>
+
+    )
+}
+
+export default CustomListItem
+
+const styles = StyleSheet.create({})
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { ScrollView } from 'react-native'
 import { SafeAreaView } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
+import CustomListItem from '../components/CustomListItem'
 
 const HomeScreen = () => {
     return (
         <SafeAreaView>
-            <Text>Welcome to HomePage!</Text>
+            <ScrollView>
+                <CustomListItem />
+            </ScrollView>
         </SafeAreaView>
     )
 }
