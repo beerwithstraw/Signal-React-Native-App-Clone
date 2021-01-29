e313a87608a71a7b023fe2151def5b5c83e1c9e6
beerwithstraw <agarwalp@tcd.ie>
2021-01-29 00:23:35 +0000

    Firebase Auth & Database Initialize

--- firebase.js
+++ firebase.js
@@ -1,3 +1,8 @@
+import * as firebase from 'firebase'
+
+import "firebase/firestore"
+import "firebase/auth"
+
 const firebaseConfig = {
     apiKey: "AIzaSyDGKfp_6BLbJWo9CHmc0FWngGUJ4PNzlgo",
     authDomain: "signal-react-native-clone.firebaseapp.com",
@@ -6,3 +11,16 @@ const firebaseConfig = {
     messagingSenderId: "345481180431",
     appId: "1:345481180431:web:a78820ad0b773c8d087a7a"
   };
+
+  let app;
+
+  if  (firebase.apps.length === 0) {
+    app = firebase.initializeApp(firebaseConfig)
+  } else {
+    app = firebase.app()
+  }
+
+  const db = app.firestore();
+  const auth = firebase.auth();
+
+  export { db, auth };
