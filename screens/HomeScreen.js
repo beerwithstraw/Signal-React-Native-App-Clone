5bc81ff3bd8a0b40a213a3457ef7617fa2d5a3dd
beerwithstraw <agarwalp@tcd.ie>
2021-01-30 01:32:46 +0000

    Home Screen Header Right

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,27 +1,49 @@
 import React, { useLayoutEffect } from 'react'
-import { ScrollView } from 'react-native'
-import { SafeAreaView } from 'react-native'
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, SafeAreaView, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import { Avatar } from 'react-native-elements'
+import { AntDesign, SimpleLineIcons } from '@expo/vector-icons'
 import { auth, db } from '../firebase'
 import CustomListItem from '../components/CustomListItem'
-import { TouchableOpacity } from 'react-native'
 
 const HomeScreen = ({ navigation }) => {
+
+    const signOutUer = () => {
+        auth.signOut().then(() => {
+            navigation.replace('Login')
+        })
+    }
 
     useLayoutEffect(() => {
         navigation.setOptions({
             title: "Signal",
-            headerStyle: { backgroundColor: "black" },
-            headerTitleStyle: { color: "white"},
-            headerTintColor: "white",
-            headerLeft: () => <View style={{marginLeft: 15}}>
-                <TouchableOpacity>
-                    <Avatar rounded source={{ uri: auth?.currentUser?.photoURL}}/>
-                </TouchableOpacity>
-            </View>
+            headerStyle: { backgroundColor: "#fff" },
+            headerTitleStyle: { color: "black"},
+            headerTintColor: "black",
+            headerLeft: () => (
+                <View style={{marginLeft: 15}}>
+                    <TouchableOpacity onPress={signOutUer} activeOpacity={0.5}>
+                        <Avatar rounded source={{ uri: auth?.currentUser?.photoURL}}/>
+                    </TouchableOpacity>
+                </View>
+            ),
+            headerRight: () => (
+                <View style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    width: 70,
+                    marginRight: 20,
+                
+                }}>
+                    <TouchableOpacity activeOpacity={0.5}>
+                        <AntDesign name="camerao" size={24} color="black" />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate('AddChat')} activeOpacity={0.5}>
+                        <SimpleLineIcons name="pencil" size={24} color="black" />
+                    </TouchableOpacity>
+                </View>
+            )
         })
-    }, [])
+    }, [navigation])
 
     return (
         <SafeAreaView>
