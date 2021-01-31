eb39fb14007090a3a9d57ff65847c5f8b4896300
beerwithstraw <agarwalp@tcd.ie>
2021-01-31 00:42:43 +0000

    Personalised Chat Screen

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import HomeScreen from './screens/HomeScreen';
 import AddChatScreen from './screens/AddChatScreen';
+import ChatScreen from './screens/ChatScreen'
 
 
 const Stack = createStackNavigator(); 
@@ -28,6 +29,9 @@ export default function App() {
         <Stack.Screen name='Register' component={RegisterScreen} />
         <Stack.Screen name='Home' component={HomeScreen} />
         <Stack.Screen name='AddChat' component={AddChatScreen} />
+        <Stack.Screen name='Chat' component={ChatScreen} />
+
+
 
 
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -57,11 +57,18 @@ const HomeScreen = ({ navigation }) => {
         })
     }, [navigation])
 
+    const enterChat = (id, chatName) => {
+        navigation.navigate('Chat', {
+            id: id,
+            chatName: chatName,
+        })
+    }
+
     return (
         <SafeAreaView>
             <ScrollView style={styles.container}>
                 {chats.map(({ id, data: { chatName } }) => (
-                    <CustomListItem key={id} id={id} chatName={chatName}/>
+                    <CustomListItem key={id} id={id} chatName={chatName} enterChat={enterChat}/>
                 ))}
             </ScrollView>
         </SafeAreaView>
