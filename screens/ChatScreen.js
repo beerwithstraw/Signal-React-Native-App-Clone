93bf0193dc260b2ecf3e7d7510b26b59affac409
beerwithstraw <agarwalp@tcd.ie>
2021-01-31 19:56:30 +0000

    Chat Screen Header

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,10 +1,34 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { Avatar } from 'react-native-elements';
+import { AntDesign, FontAwesome, Ionicons } from '@expo/vector-icons'
 
-const ChatScreen = () => {
+const ChatScreen = ({ navigation, route}) => {
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            // title: "Chat",
+            headerBackTitleVisible: false,
+            headerTitleAlign: "left",
+            headerTitle: () => (
+                <View style={{flexDirection: "row", alignItems: "center",}}>
+                    <Avatar rounded source={{ uri: "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png"}}/>
+                    <Text style={{color: "white", marginLeft: 10, fontWeight: "700", fontSize: 18}}>
+                        {route.params.chatName}
+                    </Text>
+                </View>
+            ),
+            headerLeft: () => (
+                <TouchableOpacity style={{ marginLeft: 10}} onPress={navigation.goBack}>
+                    <AntDesign name="arrowleft" size={24} color="white"/>
+                </TouchableOpacity>
+            ), 
+        })
+    }, [navigation])
+
     return (
         <View>
-            <Text>Chat Screen</Text>
+            <Text>{route.params.chatName} </Text>
         </View>
     )
 }
